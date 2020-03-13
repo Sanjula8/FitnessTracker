64249486b63edf06f17f6feee409df06bd3e307d
Sanjula <sanjula8@gmail.com>
2020-03-12 22:03:46 -0500

    fixed error in api.js

--- routes/api.js
+++ routes/api.js
@@ -1,36 +1,35 @@
 const Workout = require("../models/workout");
 
-module.exports -
-	function(app) {
-		app.get("/api/workouts", function(req, res) {
-			Workout.find()
-				.then(data => {
-					res.json(data);
-				})
-				.catch(err => {
-					res.json(err);
-				});
-		});
-
-		app.post("/api/worouts", function(req, res) {
-			Workout.create({})
-				.then(data => res.json(data))
-				.catch(err => {
-					console.log(err);
-					res.json(err);
-				});
-
-			app.put("api/workouts/:id", ({ body, params }, res) => {
-				Workout.findByIDAndUpdate(
-					params.id,
-					{ $push: { exercises: body } },
-					{ new: true, runValidators: true }
-				);
+module.exports = function(app) {
+	app.get("/api/workouts", function(req, res) {
+		Workout.find()
+			.then(data => {
+				res.json(data);
 			})
-				.then(data => res.json(data))
-				.catch(err => {
-					console.log(err);
-					res.json(err);
-				});
-		});
-	};
+			.catch(err => {
+				res.json(err);
+			});
+	});
+
+	app.post("/api/worouts", function(req, res) {
+		Workout.create({})
+			.then(data => res.json(data))
+			.catch(err => {
+				console.log(err);
+				res.json(err);
+			});
+
+		app.put("api/workouts/:id", ({ body, params }, res) => {
+			Workout.findByIDAndUpdate(
+				params.id,
+				{ $push: { exercises: body } },
+				{ new: true, runValidators: true }
+			);
+		})
+			.then(data => res.json(data))
+			.catch(err => {
+				console.log(err);
+				res.json(err);
+			});
+	});
+};
